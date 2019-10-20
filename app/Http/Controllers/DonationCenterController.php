7c06af0b4cb43c05ee6343da7d1be20d4b8854af
starno <starnleymbote@gmail.com>
2019-10-20 10:40:19 +0300

    model update

---
+++ app/Http/Controllers/DonationCenterController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\DonationCenter;
+use Illuminate\Http\Request;
+
+class DonationCenterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\DonationCenter  $donationCenter
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DonationCenter $donationCenter)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\DonationCenter  $donationCenter
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DonationCenter $donationCenter)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\DonationCenter  $donationCenter
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DonationCenter $donationCenter)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\DonationCenter  $donationCenter
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DonationCenter $donationCenter)
+    {
+        //
+    }
+}
