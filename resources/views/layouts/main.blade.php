a7c24eeee8890764b7758b963c232e34ffb88968
starno <starnleymbote@gmail.com>
2019-11-19 09:10:44 +0300

    added the link to the home page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -87,7 +87,7 @@
                                 @if (Auth::User()->role_id == 1)
                                 <li class="list-group-item">
 
-                                        <a href="/index">Home</a>
+                                        <a href="/user_index">Home</a>
 
                                     </li>
 
